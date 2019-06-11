d06ade985316684ba2623352ef364388d76b3e4f
Nati_Shchiglik <natishch94@gmail.com>
2019-06-11 19:13:32 +0300

    begining of the second page

--- kotelMania/app/src/main/java/com/example/kotelmania/listActivity.java
+++ kotelMania/app/src/main/java/com/example/kotelmania/listActivity.java
@@ -2,12 +2,21 @@ package com.example.kotelmania;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class listActivity extends AppCompatActivity {
+
+    ListView lst;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        lst = (ListView) findViewById(R.id.NoteList);
+        ArrayAdapter<T> arrayAdapter = new ArrayAdapter<T>()
+
     }
 }
